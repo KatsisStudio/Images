b1f63402c6bef4fd2b4d904645874c95071c65ad
Xwilarg <xwilarg@protonmail.com>
2024-05-15 18:34:58 +0200

    Add preview page

--- index.php
+++ index.php
@@ -8,15 +8,44 @@ use Twig\Environment;
 $loader = new FilesystemLoader(["templates"]);
 $twig = new Environment($loader);
 
-
 $metadata = json_decode(file_get_contents("data/info.json"), true);
-$images = [];
 
-foreach ($metadata as $info)
+function showAll()
 {
-    array_push($images, [ "id" => $info["id"], "extension" => $info["format"]]);
+    $images = [];
+    
+    foreach ($metadata as $info)
+    {
+        array_push($images, [ "id" => $info["id"], "extension" => $info["format"]]);
+    }
+    
+    echo $twig->render("index.html.twig", [
+        "css" => "index",
+        "images" => $images
+    ]);
 }
 
-echo $twig->render("index.html.twig", [
-    "images" => $images
-]);
+if (isset($_GET["id"]))
+{
+    $isOk = false;
+
+    foreach ($metadata as $info) {
+        if ($_GET["id"] === $info["id"]) {
+            
+            echo $twig->render("page.html.twig", [
+                "css" => "page",
+                "metadata" => $info
+            ]);
+            break;
+        }
+    }
+
+    if (!$isOk)
+    {
+        showAll();
+    }
+}
+else
+{
+    showAll();
+}
