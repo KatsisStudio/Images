955e685a62b2e081f58c43928fa3c3d922e5195f
Xwilarg <xwilarg@protonmail.com>
2024-11-15 17:34:23 +0100

    Update website

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once "vendor/autoload.php";
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-$loader = new FilesystemLoader(["templates"]);
+$loader = new FilesystemLoader(["templates", "common/templates"]);
 $twig = new Environment($loader);
 
 $urlData = array_filter(explode("/", substr(explode("?", $_SERVER["REQUEST_URI"])[0], 1)));
